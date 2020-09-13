da177106a0075955633f0704f54a2a397e581beb
Chetan Nellekeri <chetan.nellekeri@tivo.com>
2020-09-13 19:06:21 +0530

    Template design pattern base example -- Behavioral design pattern

--- src/main/java/com/designpatterns/behavioral/baseexample/Record.java
+++ src/main/java/com/designpatterns/behavioral/baseexample/Record.java
@@ -1,5 +1,12 @@
 package com.designpatterns.behavioral.baseexample;
 
+/**
+ * Template design pattern is one among behavioral design pattern
+ * Defn: Template design pattern is to define an algorithm as a skeleton of operations and leave the details to be implemented by the child classes
+ *
+ * In this example save() is an template method. beforeSave() and afterSave() is a operations.
+ *
+ */
 public abstract class Record {
 
     public boolean save() {
